63a6ead01dbe60eac87aa43ea8bcefb86822ff5c
RFirdaus21 <mraihanf11@gmail.com>
2026-02-25 08:17:42 +0700

    first

---
+++ backend/src/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import { ENV } from './config/env';
+import { clerkMiddleware }  from '@clerk/express';
+
+const app = express();
+
+app.use(cors({ origin: ENV.FRONTEND_URL }));
+app.use(clerkMiddleware());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+
+    // const{} = req.body;
+
+    res.json({ 
+        message: "Welcome to TokoShop API - Powered by PostgreSQL, Drizzle ORM, and Clerk Auth",
+        endpoints: {
+            users: "/api/users",
+            products: "/api/products",
+            comments: "/api/comments",
+        }
+    });
+})
+
+app.listen(ENV.PORT, () => console.log('Server is running on PORT: ',ENV.PORT));
